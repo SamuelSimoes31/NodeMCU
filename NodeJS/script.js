d1961a535980b2c463673975627b9a1864a05c32
Samuel Simões <samuelsimoes31@gmail.com>
2020-05-26 21:52:50 -0300

    Enviando string por input

--- NodeJS/script.js
+++ NodeJS/script.js
@@ -1,8 +1,9 @@
 const botaoTeste = document.getElementById('teste')
 botaoTeste.onclick = e => {
+    const inputTexto = document.getElementsByName('testeTexto')
     $.ajax({
         url: 'teste',
-        data: {string: 'AAAAA'},
+        data: {string: inputTexto[0].value},
         success(data){
             console.log(data)
         }
