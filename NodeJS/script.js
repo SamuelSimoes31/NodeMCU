88905e5735cf77d530a1c0b40312d68e53d1550a
Samuel Simões <samuelsimoes31@gmail.com>
2020-05-26 23:16:30 -0300

    3 sliders

--- NodeJS/script.js
+++ NodeJS/script.js
@@ -1,20 +1,37 @@
-const botaoTeste = document.getElementById('teste')
-botaoTeste.onclick = e => {
-    const inputTexto = document.getElementsByName('testeTexto')
-    $.ajax({
-        url: 'teste',
-        data: {string: inputTexto[0].value},
-        success(data){
-            console.log(data)
-        }
-    })
+// const botaoTeste = document.getElementById('teste')
+// botaoTeste.onclick = e => {
+//     const inputTexto = document.getElementsByName('testeTexto')
+//     $.ajax({
+//         url: 'teste',
+//         data: {string: inputTexto[0].value},
+//         success(data){
+//             console.log(data)
+//         }
+//     })
 
-}
+// }
 
-const sliderRed = document.getElementById('red')
-sliderRed.oninput = () => {
-    $.ajax({
-        url: 'teste',
-        data: {string: sliderRed.value}
-    })
-}
+// const sliderRed = document.getElementById('red')
+// sliderRed.oninput = () => {
+//     $.ajax({
+//         url: 'teste',
+//         data: {string: sliderRed.value}
+//     })
+// }
+
+const sliders = document.querySelectorAll('#sliders div')
+sliders.forEach(e => {
+    const slider = e.children[0]
+    const p = e.children[1]
+    // console.log(slider,p)
+    slider.oninput = () => {
+        p.innerHTML = slider.value
+        $.ajax({
+            url: 'rgb',
+            data : {
+                color: slider.id,
+                value: slider.value
+            }
+        })
+    }
+})
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -8,4 +8,9 @@ app.get('/teste', (req,res) => {
     res.send(req.query.string)
 })
 
+app.get('/rgb',(req,res) => {
+    console.log(req.query.color,req.query.value)
+    res.end()
+})
+
 app.listen(8080, () => console.log('Executando na porta 8080...'))
